5dca9f1642a79f96a17bca59388ca0436dcc574a
Pruthvi Raj <pruthvi992@github.com>
2018-05-09 13:02:55 -0700

    some more

--- flaskapp.py
+++ flaskapp.py
@@ -2,13 +2,14 @@ from flask import Flask, jsonify
 import logging
 application = Flask(__name__)
 
-_logger = logging.getLogger('mainLogger')
-lfhdlr = logging.FileHandler('/var/log/gumgum/flask_blog_app.log')
-_logger.addHandler(lfhdlr)
-_logger.setLevel(logging.INFO)
+logger = logging.getLogger('mainLogger')
+logger.addHandler(logging.FileHandler('/var/log/gumgum/flask_blog_app.log'))
+logger.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+logger.setLevel(logging.WARNING)
 
 @application.route("/")
 def hello():
+    logger.debug('Instantiating definition.')
     return "<h1 style='color:blue'>Hello World</h1>"
 
 @application.route("/health")
@@ -16,5 +17,5 @@ def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == "__main__":
-    _logger.info('Starting application.')
+    logger.info('Starting application.')
     application.run(host='0.0.0.0')
